407c4fe88c3bbac7c6e5b0290b02f1ab0578214f
Pavel Kardash <cardash@mail.ru>
2022-07-31 18:54:59 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar, Text, useColorScheme} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <NavigationContainer>
+      <SafeAreaView style={backgroundStyle}>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <Text>Hello</Text>
+      </SafeAreaView>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ src/Provider.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+type ProviderPropsType = {};
+
+export const Provider = ({}: ProviderPropsType) => {
+  return <View style={styles.container} />;
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
