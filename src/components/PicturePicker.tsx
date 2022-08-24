5e0c95e78b9826797b1730cdfc49b489e5fb2088
Pavel Kardash <cardash@mail.ru>
2022-08-24 18:28:16 +0300

    add modal footer

--- src/components/PicturePicker.tsx
+++ src/components/PicturePicker.tsx
@@ -1,7 +1,8 @@
-import React, { ForwardedRef } from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import RBSheet from "react-native-raw-bottom-sheet";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import colors from "../../assets/theme/colors";
 
 
 type PicturePickerPropsType = {
@@ -13,11 +14,11 @@ type PicturePickerPropsType = {
 export const PicturePicker = React.forwardRef(({ onClose, onOpen }: PicturePickerPropsType, ref) => {
   const options = [
     {
-      name: "Take photo from a camera", icon: <MaterialCommunityIcons size={24} name={"camera"} />, onPress: () => {
+      name: "Take photo from a camera", icon: <MaterialCommunityIcons style={styles.icon}  color={colors.grey}   size={24} name={"camera"} />, onPress: () => {
       },
     },
     {
-      name: "Choose from gallery", icon: <MaterialCommunityIcons size={24} name={"view-gallery"} />, onPress: () => {
+      name: "Choose from gallery", icon: <MaterialCommunityIcons style={styles.icon} color={colors.grey}  size={24} name={"view-gallery"} />, onPress: () => {
       },
     },
   ];
@@ -25,25 +26,40 @@ export const PicturePicker = React.forwardRef(({ onClose, onOpen }: PicturePicke
     <RBSheet
       // @ts-ignore
       ref={ref}
-      height={300}
+      height={200}
       openDuration={250}
+      closeOnDragDown
       customStyles={{
         container: {
-          justifyContent: "center",
-          alignItems: "center",
+          borderTopRightRadius: 20,
+          borderTopLeftRadius: 20,
         },
       }}
     >
-      {options.map(({ name, onPress, icon }) =>
-        <TouchableOpacity key={name}>
-          {icon}
-          {<Text>{name}</Text>}
-        </TouchableOpacity>,
-      )}
+      <View style={styles.pickerContainer}>
+        {options.map(({ name, onPress, icon }) =>
+          <TouchableOpacity style={styles.picker} key={name}>
+            {icon}
+            {<Text style={{fontSize:18}}>{name}</Text>}
+          </TouchableOpacity>,
+        )}
+      </View>
     </RBSheet>
   );
 });
 
 const styles = StyleSheet.create({
   container: {},
+  icon: {
+    marginRight:15,
+  },
+  pickerContainer: {
+    paddingHorizontal:20
+  },
+  picker: {
+    flexDirection: "row",
+    paddingTop: 20,
+    alignItems:"center",
+  },
+
 });
